2d5309319627272d6a040a66bbf948b4706593d0
abhipin <abhinavpin@gmail.com>
2022-03-08 15:36:24 +0530

    Added more instructions

--- reader.cpp
+++ reader.cpp
@@ -1,5 +1,16 @@
 #include "reader.hpp"
 
+// function to flip bool
+bool flip(bool *val) {
+    bool dref = *val;
+    if (dref) {
+        *val = false;
+        return false;
+    } else { 
+        *val = true;
+        return true;
+    }
+}
 
 Opcodes Interpreter::GetOpcodes(TCHAR* array, int nSize) {
     Opcodes opcodes;
@@ -26,11 +37,47 @@ PrMessage Interpreter::ExecOpcode(POpcodes opcode) {
     for (std::vector<int>::iterator it = arrayVec.begin(); it != arrayVec.end(); it++) {
         if ((it - arrayVec.begin()) % 2) { continue; } // if iterator is at odd index then ignore iteration
         switch(*it) {
-            case 1:
-                std::cout << *(it + 0x1) << std::endl;
+            case 1: // output
+            {
+                int outval =  sVar[sVarPtr] ? sVar[sVarPtr] : *(it + 0x1); // use custom val if selected loc is null, otherwise use selected loc
+                if (!TextMode) {
+                    std::cout << outval;
+                } else {
+                    std::cout << (char)(96 + outval); // 96 is ascii offset 
+                }
                 break;
+            }
+            case 2: // get input
+            {
+                char buf[MAX_INPUT];
+                std::cin >> buf;
+                int val = *(it + 0x1);
+                if (sVarPtr) { sVar[sVarPtr] = val; } // set val if pointer is not at 0th loc
+                break;
+            }
+            case 3: // select loc
+            {
+                int selector = *(it + 0x1) - 1;
+                sVarPtr = selector;
+                break;
+            }
+            case 4: // int/ascii mode
+            {
+                flip(&TextMode);
+                break;
+            }
+            case 5: // clear val location
+            {
+                sVar[sVarPtr] = 0x0;
+                break;
+            }
             default:
                 break;
+            case 6: // newline
+            {
+                std::cout << "\n";
+                break;
+            }
         }
     }
     return message;
--- reader.hpp
+++ reader.hpp
@@ -1,5 +1,7 @@
 #include "main.hpp"
 
+#define MAX_INPUT 4096
+
 typedef struct {
     std::vector<int> OpcodesArray;
     int nSize;
@@ -14,4 +16,8 @@ class Interpreter {
     public:
         Opcodes GetOpcodes(TCHAR* array, int nSize);
         PrMessage ExecOpcode(POpcodes opcode);
+    private:
+        bool TextMode = false;
+        int sVar[2048] = { 0 };
+        int sVarPtr = 0;
 };
